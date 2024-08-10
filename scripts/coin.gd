1e7dd2a64132754ef2d2621cc572d4e31e0265b4
geraldib-dev <geraldib.dev@gmail.com>
2024-08-10 23:38:28 +0200

    added ui and music

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
-
+@onready var game_manager = %GameManager
+@onready var score_count = %ScoreCount
 
 func _on_body_entered(body):
-	print("catched coin!")
+	game_manager.add_point()
+	score_count.text = str("X ", game_manager.score)
 	queue_free()
